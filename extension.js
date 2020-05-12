7d727a6094826dcfa690be403acce6aab3f450ec
Rusu Traian Cristian <RusuTraianCristian@users.noreply.github.com>
2020-05-12 11:21:45 +0300

    Add files via upload

--- extension.js
+++ extension.js
@@ -1,20 +1,25 @@
 const clientId = 'ptm1pmj69bfuceli46nae7bfhi7q8n';
-
-const logExt = () => {
-    const getURL = `https://api.twitch.tv/extensions/${clientId}/live_activated_channels`;
-    const header = {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Client-Id': clientId
-        }
+const url = `https://api.twitch.tv/extensions/${clientId}/live_activated_channels`;
+const options = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Client-Id': clientId
     }
-    fetch(getURL, header)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => console.error(error));
-
 }
+
+const logExt = document.getElementById("logExt");
+logExt.onclick = async () => {
+    try {
+        const res = await fetch(url, options);
+        const data = await res.json();
+        console.log("This is the data we got in response: ", data);
+    } catch (err) {
+        console.error(err);
+    } finally {
+        console.log("Request done.");
+    }
+}
+
+    
